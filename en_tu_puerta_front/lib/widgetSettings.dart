157975a902137db81def736623dbe1bee4d2d990
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2025-02-16 19:20:59 -0400

    Barra de navegacion inferior
    Se hizo la barra de navegación inferior que llevan a las diferentes vistas

---
+++ en_tu_puerta_front/lib/widgetSettings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WidgetSettings extends StatelessWidget {
+  const WidgetSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: Text('Index 3: Settings'),
+      
+    );
+  }
+}
