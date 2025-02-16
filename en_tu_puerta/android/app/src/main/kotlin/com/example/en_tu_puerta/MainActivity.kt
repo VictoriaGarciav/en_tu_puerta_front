79961868b62f052c8c08bbbbb0c6f314108d1f86
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2025-02-16 18:03:32 -0400

    Programa de prueba

--- en_tu_puerta/android/app/src/main/kotlin/com/example/en_tu_puerta/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.en_tu_puerta
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ en_tu_puerta_front/android/app/src/main/kotlin/com/example/en_tu_puerta_front/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.en_tu_puerta_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
