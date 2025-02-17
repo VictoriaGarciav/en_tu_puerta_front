06cff89115cc54a3a525bdf154978438fed6a7a5
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2025-02-17 00:05:33 -0400

    Searchbar continuacion
    Ahora hay una search bar y un peque;o filtro de muestra

--- en_tu_puerta_front/lib/widget_search.dart
+++ en_tu_puerta_front/lib/widget_search.dart
@@ -4,11 +4,57 @@ class WidgetSearch extends StatelessWidget {
   const WidgetSearch({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
-      child: Text('Index 1: Search'),
-      
-    );
-  }
+Widget build(BuildContext context) {
+  return Container(
+    color: Colors.grey,
+    child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+          child: SearchBar(
+            hintText: 'Search...',
+            leading: Icon(Icons.search),
+            trailing: [
+              PopupMenuButton<String>(
+                icon: Icon(Icons.filter_list),
+                onSelected: (String value) {
+                  // Handle filter selection
+                  switch (value) {
+                    case 'Todos':
+                      // Handle Todos filter
+                      break;
+                    case 'Servicio':
+                      // Handle Servicio filter
+                      break;
+                    case 'Cuenta':
+                      // Handle Cuenta filter
+                      break;
+                  }
+                },
+                itemBuilder: (BuildContext context) => [
+                  PopupMenuItem<String>(
+                    value: 'Todos',
+                    child: Text('Todos'),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Servicio',
+                    child: Text('Servicio'),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Cuenta',
+                    child: Text('Cuenta'),
+                  ),
+                ],
+              ),
+            ],
+            onChanged: (value) {
+              // Handle search input
+            },
+          ),
+        ),
+        Text('Index 1: Search'),
+      ],
+    ),
+  );
+}
 }
