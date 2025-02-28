7d6a55270a3ff4b9bf56cc67e355dea0c1a357d3
VictoriaGarciav <113353097+VictoriaGarciav@users.noreply.github.com>
2025-02-28 17:33:13 -0400

    Prueba Fecha 📅

--- en_tu_puerta_front/lib/pages/home_page.dart
+++ en_tu_puerta_front/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,6 +11,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController timePicker = TextEditingController();
   TextEditingController datePicker = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +60,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.purple,
               ), //TextStyle
             ), //Input Decoration
-          ),
+            onTap: () async {
+              DateTime? datetime = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1950),
+                lastDate: DateTime(2100));
+              
+              if (datetime != null) {
+                String formattedDate = 
+                    DateFormat('yyyy-MM-dd').format(datetime);
+
+                  setState(() {
+                    datePicker.text = formattedDate;
+                  });
+              }
+            },
+          ), //Text Field
 
         ],
       ), // Column 
